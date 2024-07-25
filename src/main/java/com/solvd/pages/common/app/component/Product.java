9d0885a5c524f962f701ac94ad9cd019f5e0b48e
MariuszSolvd <mkaminski@solvd.com>
2024-07-25 12:28:53 +0200

    Added click on product with ProductDetailsPage return

--- src/main/java/com/solvd/pages/common/app/component/Product.java
+++ src/main/java/com/solvd/pages/common/app/component/Product.java
@@ -1,5 +1,6 @@
 package com.solvd.pages.common.app.component;
 
+import com.solvd.pages.common.app.ProductDetailsPage;
 import com.zebrunner.carina.webdriver.gui.AbstractUIObject;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
@@ -15,4 +16,9 @@ public abstract class Product extends AbstractUIObject {
     public abstract String getPriceText();
 
     public abstract void clickAddCart();
+
+    public ProductDetailsPage clickOnProduct() {
+        click();
+        return initPage(getDriver(), ProductDetailsPage.class);
+    }
 }
