06cf810ef16ed1fef16189a8f04adc48b63ad360
MariuszSolvd <mkaminski@solvd.com>
2024-07-25 13:29:40 +0200

    Move repetitive code to Product, rename fields(avoid Carina name collision), add method to swipe if cannot see field

--- src/main/java/com/solvd/pages/common/app/component/Product.java
+++ src/main/java/com/solvd/pages/common/app/component/Product.java
@@ -1,24 +1,42 @@
 package com.solvd.pages.common.app.component;
 
 import com.solvd.pages.common.app.ProductDetailsPage;
+import com.zebrunner.carina.utils.mobile.IMobileUtils;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractUIObject;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 
 
-public abstract class Product extends AbstractUIObject {
+public abstract class Product extends AbstractUIObject implements IMobileUtils {
     public Product(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
     }
 
-    public abstract String getNameText();
+    public abstract ExtendedWebElement getProductName();
 
-    public abstract String getPriceText();
+    public abstract ExtendedWebElement getProductPrice();
 
     public abstract void clickAddCart();
+
+    public String getNameText() {
+        swipeIfNotVisible(getProductName());
+        return getProductName().getText();
+    }
+
+    public String getPriceText() {
+        swipeIfNotVisible(getProductPrice());
+        return getProductPrice().getText();
+    }
 
     public ProductDetailsPage clickOnProduct() {
         click();
         return initPage(getDriver(), ProductDetailsPage.class);
+    }
+
+    private void swipeIfNotVisible(ExtendedWebElement element) {
+        if (!element.isVisible(0)) {
+            swipe(element);
+        }
     }
 }
