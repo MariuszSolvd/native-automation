19ad617c906eba5f2e40a87e72f6dea7b8ed2a7c
MariuszSolvd <mkaminski@solvd.com>
2024-07-29 14:54:26 +0200

    Added CheckoutInfoPage and Button in CartPage to it

---
+++ src/main/java/com/solvd/pages/common/app/CheckoutInfoPage.java
@@ -0,0 +1,9 @@
+package com.solvd.pages.common.app;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class CheckoutInfoPage extends RegularsPage{
+    public CheckoutInfoPage(WebDriver driver) {
+        super(driver);
+    }
+}
