a0aa7c69dcd45aa397d3df129034a3f93b5dd0b0
MariuszSolvd <mkaminski@solvd.com>
2024-07-29 13:20:15 +0200

    Change IOS_HEADER to ClassChain

--- src/main/java/com/solvd/utilis/Constants.java
+++ src/main/java/com/solvd/utilis/Constants.java
@@ -2,7 +2,7 @@ package com.solvd.utilis;
 
 public final class Constants {
     public static final String HEADER_ANDROID = "//android.view.ViewGroup[@content-desc='test-Menu']/parent::android.view.ViewGroup";
-    public static final String HEADER_IOS = "//XCUIElementTypeOther[@name='headerContainer']/parent::XCUIElementTypeOther";
+    public static final String HEADER_IOS = "**/XCUIElementTypeOther[$type == 'XCUIElementTypeOther' AND name == 'headerContainer'$][-2]";
     public static final String FOOTER_ANDROID = "//android.widget.TextView[@text='\uF099']//parent::android.view.ViewGroup";
     public static final String FOOTER_IOS = "**/XCUIElementTypeOther[`name == \"\uF099 \uF09A \uF0D5 \uF0E1 © 2024 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy\"`][2]";
 }
